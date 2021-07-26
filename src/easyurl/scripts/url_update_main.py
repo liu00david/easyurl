4fe7efa269f558b8f11031197eef7b10b7f0b487
David Liu <liu00david@gmail.com>
2021-07-26 05:05:55 -0400

    good; ready to start db for url storage/query

--- src/easyurl/scripts/url_update_main.py
+++ src/easyurl/scripts/url_update_main.py
@@ -1,15 +1,27 @@
+import argparse
+
 from easyurl.url_db_update import UrlDatabaseFuncs
 
 
-def main():
+def start_url(args):
     """
-    12753774 is the max depair input index for 99,99 nouns and adjs
+    12,753,774 is the max depair input index for 99,99 nouns and adjs
     pair(0,5049)
+    Say that theres a cap of 12,000,000
 
     """
-    all_funcs = UrlDatabaseFuncs()
-    print(all_funcs.url)
+    UrlDatabaseFuncs("hi.com")
 
+def main():
+    parser = argparse.ArgumentParser(description='Update wordlist DB.')
+    # parser.add_argument('action', type=str, choices=["add","replace","reset"],
+    #                     help='Choose the action.')
+    # parser.add_argument('pos', type=str, choices=["nouns","adjectives"],
+    #                     help='Choose the part of speech.')
+
+    args = parser.parse_args()
+
+    start_url(args)
 
 if __name__ == '__main__':
     main()
